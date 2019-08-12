919b883a22c90318e32cf8f55fac1bcd687dbb70
lishunrong-ds <123.gome>
2019-08-12 11:04:08 +0800

    赵雷的歌

--- leisong/sign.py
+++ leisong/sign.py
@@ -20,10 +20,10 @@ def rsa(random,public_key,data):
 
 
 def sign(data):
-    random = '1' * 16
+    random = '1' * 16 # 随机数
     text = json.dumps(data)
-    params = aes(text,'0CoJUm6Qyw8W8jud')
-    params = aes(params,random)
+    params = aes(text,'0CoJUm6Qyw8W8jud')#第一次加签
+    params = aes(params,random)#第二次加签
     encSecKey = rsa(random.encode(),"010001",'00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7')
     return  {'params':params,
              'encSecKey':encSecKey  }
